7ef053d55f5b7d0ceab44931880896ee10ef4382
Torsten Knorr <create-soft@freenet.de>
2008-10-05 14:35:48 +0000

    Import of KNORR/Net-DownloadMirror-0.10 from CPAN.
    gitpan-cpan-distribution: Net-DownloadMirror
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         KNORR/Net-DownloadMirror-0.10.tar.gz
    gitpan-cpan-author:       KNORR
    gitpan-cpan-maturity:     released

--- lib/Net/DownloadMirror.pm
+++ lib/Net/DownloadMirror.pm
@@ -30,14 +30,14 @@
  use Storable;
 #------------------------------------------------
  @Net::DownloadMirror::ISA = qw(Net::MirrorDir);
- $Net::DownloadMirror::VERSION = '0.09';
+ $Net::DownloadMirror::VERSION = '0.10';
 #-------------------------------------------------
  sub _Init
  	{
  	my ($self, %arg) = @_;
- 	tie($self->{_filename}, "Net::DownloadMirror::FileName", $self);
- 	$self->{_filename}		= $arg{filename}	|| "lastmodified_remote";
- 	$self->{_delete}		= $arg{delete}	|| "disabled";
+ 	tie($self->{_filename}, 'Net::DownloadMirror::FileName', $self);
+ 	$self->{_filename}		= $arg{filename}	|| 'lastmodified_remote';
+ 	$self->{_delete}		= $arg{delete}	|| 'disabled';
  	$self->{_current_modified}	= {};
  	return 1;
  	}
@@ -49,48 +49,49 @@
  	my ($rh_lf, $rh_ld) = $self->ReadLocalDir();
  	if($self->{_debug})
  		{
- 		print("local files : $_\n") for(sort keys %{$rh_lf});
- 		print("local dirs : $_\n") for(sort keys %{$rh_ld});
+ 		print("local files : $_\n") for(sort keys %$rh_lf);
+ 		print("local dirs : $_\n") for(sort keys %$rh_ld);
  		}
  	my ($rh_rf, $rh_rd) = $self->ReadRemoteDir();
  	if($self->{_debug})
  		{
- 		print("remote files : $_\n") for(sort keys %{$rh_rf});
- 		print("remote dirs : $_\n") for(sort keys %{$rh_rd});
+ 		print("remote files : $_\n") for(sort keys %$rh_rf);
+ 		print("remote dirs : $_\n") for(sort keys %$rh_rd);
  		}
  	my $ra_rdnil = $self->RemoteNotInLocal($rh_ld, $rh_rd);
  	if($self->{_debug})
  		{
- 		print("remote directories not in local: $_\n") for(@{$ra_rdnil});
+ 		print("remote directories not in local: $_\n") for(@$ra_rdnil);
  		}
  	$self->MakeDirs($ra_rdnil);
  	my $ra_rfnil = $self->RemoteNotInLocal($rh_lf, $rh_rf);
  	if($self->{_debug})
  		{
- 		print("remote files not in local : $_\n") for(@{$ra_rfnil});
+ 		print("remote files not in local : $_\n") for(@$ra_rfnil);
  		}
  	$self->StoreFiles($ra_rfnil);
- 	my $ra_mrf = $self->CheckIfModified($rh_rf);
- 	if($self->{_debug})
- 		{
- 		print("modified remote files : $_\n") for(@{$ra_mrf});
- 		}
- 	$self->StoreFiles($ra_mrf);
- 	if($self->{_delete} eq "enable")
+ 	if($self->{_delete} eq 'enable')
  		{
  		my $ra_lfnir = $self->LocalNotInRemote($rh_lf, $rh_rf);
  		if($self->{_debug})
  			{
- 			print("local files not in remote: $_\n") for(@{$ra_lfnir});
+ 			print("local files not in remote: $_\n") for(@$ra_lfnir);
  			}
  		$self->DeleteFiles($ra_lfnir);
  		my $ra_ldnir = $self->LocalNotInRemote($rh_ld, $rh_rd);
  		if($self->{_debug})
  			{
- 			print("local directories not in remote : $_\n") for(@{$ra_ldnir});
+ 			print("local directories not in remote : $_\n") for(@$ra_ldnir);
  			}
  		$self->RemoveDirs($ra_ldnir);
  		}
+ 	delete(@{$rh_rf}{@$ra_rfnil});
+ 	my $ra_mrf = $self->CheckIfModified($rh_rf);
+ 	if($self->{_debug})
+ 		{
+ 		print("modified remote files : $_\n") for(@$ra_mrf);
+ 		}
+ 	$self->StoreFiles($ra_mrf);
  	$self->Quit();
  	return 1;
  	}
@@ -100,30 +101,53 @@
  	my ($self, $rh_rf) = @_;
  	return [] unless($self->IsConnection());
  	my @mf;
- 	for my $rf (keys(%{$rh_rf}))
+ 	my $changed = undef;
+ 	for my $rf (keys(%$rh_rf))
  		{
- next unless($self->{_current_modified}{$rf} = $self->{_connection}->mdtm($rf));
+ 		next unless($self->{_current_modified}{$rf} = $self->{_connection}->mdtm($rf));
  		if(defined($self->{_last_modified}{$rf}))
  			{
- next if($self->{_last_modified}{$rf} eq $self->{_current_modified}{$rf});
- 			} 
+ 			next if($self->{_last_modified}{$rf} eq $self->{_current_modified}{$rf});
+ 			}
+ 		else
+ 			{
+ 			$self->{_last_modified}{$rf} = $self->{_current_modified}{$rf};
+ 			$changed++;
+ 			}
  		push(@mf, $rf);
  		}
+ 	store($self->{_last_modified}, $self->{_filename}) if($changed);
  	return \@mf;
+ 	}
+#-------------------------------------------------
+ sub UpdateLastModified
+ 	{
+ 	my ($self, $ra_rf) = @_;
+ 	return unless($self->IsConnection());
+ 	my $mt;
+ 	for(@$ra_rf)
+ 		{
+ 		next unless($mt = $self->{_connection}->mdtm($_));
+ 		next unless($mt =~ m/^\d+$/);
+ 		$self->{_last_modified}{$_} = $mt;
+ 		}
+	store($self->{_last_modified}, $self->{_filename});
+ 	return 1;
  	}
 #-------------------------------------------------
  sub StoreFiles
  	{
  	my ($self, $ra_rf) = @_;
- 	return 0 unless(@{$ra_rf} && $self->IsConnection());
+ 	return 0 unless(@$ra_rf && $self->IsConnection());
  	my $lf;
- 	for my $rf (@{$ra_rf})
+ 	for my $rf (@$ra_rf)
  		{
  		$lf = $rf;
  		$lf =~ s!$self->{_regex_remotedir}!$self->{_localdir}!;
  		next unless($self->{_connection}->get($rf, $lf));
- $self->{_last_modified}{$rf} = 
- 	$self->{_current_modified}{$rf} || $self->{_connection}->mdtm($rf);
+ 		$self->{_last_modified}{$rf} = 
+ 			$self->{_current_modified}{$rf}
+ 			|| $self->{_connection}->mdtm($rf);
  		}
  	store($self->{_last_modified}, $self->{_filename});
  	return 1;
@@ -132,11 +156,13 @@
  sub MakeDirs
  	{
  	my ($self, $ra_rd) = @_;
- 	return 0 unless(@{$ra_rd});
- 	for(@{$ra_rd})
+ 	return 0 unless(@$ra_rd);
+ 	my $ld;
+ 	for my $rd (@$ra_rd)
  		{
- 		s!$self->{_regex_remotedir}!$self->{_localdir}!;
- 		mkpath($_, $self->{_debug}) unless(-d $_);
+ 		$ld = $rd;
+ 		$ld =~ s!$self->{_regex_remotedir}!$self->{_localdir}!;
+ 		mkpath($ld, $self->{_debug}) unless(-d $ld);
  		}
  	return 1;
  	}
@@ -144,9 +170,9 @@
  sub DeleteFiles
  	{
  	my ($self, $ra_lf) = @_;
- 	return 0 unless(($self->{_delete} eq "enable") && @{$ra_lf});
+ 	return 0 unless(($self->{_delete} eq 'enable') && @$ra_lf);
 	my $rf;
- 	for my $lf (@{$ra_lf})
+ 	for my $lf (@$ra_lf)
  		{
  		$rf = $lf; 
  		next unless(-f $lf);
@@ -162,15 +188,71 @@
  sub RemoveDirs
  	{
  	my ($self, $ra_ld) = @_;
- 	return 0 unless(($self->{_delete} eq "enable") && @{$ra_ld});
- 	for(@{$ra_ld})
+ 	return 0 unless(($self->{_delete} eq 'enable') && @$ra_ld);
+ 	for my $ld (@$ra_ld)
  		{
- 		next unless(-d $_);
- 		rmtree($_, $self->{_debug}, 1);
+ 		next unless(-d $ld);
+ 		rmtree($ld, $self->{_debug}, 1);
  		}
  	return 1;
  	}
 #------------------------------------------------
+ sub CleanUp
+ 	{
+ 	my ($self, $ra_exists) = @_;
+ 	my %h_temp = ();
+ 	for my $key (@$ra_exists)
+ 		{
+		if(
+ 			defined($self->{_last_modified}{$key})
+ 			&& 
+ 			($self->{_last_modified}{$key} =~ m/^\d+$/)
+ 			)
+ 			{
+ 			$h_temp{$key} = delete($self->{_last_modified}{$key});
+ 			}
+ 	 	if($self->{_debug})
+ 	 		{
+ 			print(
+ 				"key: $_	value: "
+ 				. (defined($self->{_last_modified}{$_}) ? $self->{_last_modified}{$_} : 'undef')
+ 				. " removed\n")
+					for(keys(%{$self->{_last_modified}}));
+ 			}
+ 		}
+ 	%{$self->{_last_modified}} = %h_temp;
+ 	store($self->{_last_modified}, $self->{_filename});
+ 	return 1;
+ 	}
+#-------------------------------------------------
+ sub LtoR
+ 	{
+ 	my ($self, $ra_lp) = @_;
+ 	my $ra_rp = [];
+ 	my $rp;
+ 	for(@$ra_lp)
+ 		{
+ 		$rp = $_;
+ 		$rp =~ s!$self->{_regex_localdir}!$self->{_remotedir}!;
+ 		push(@$ra_rp, $rp);
+ 		}
+ 	return $ra_rp;
+ 	}
+#-------------------------------------------------
+ sub RtoL
+ 	{
+ 	my ($self, $ra_rp) = @_;
+ 	my $ra_lp = [];
+ 	my $lp;
+ 	for(@$ra_rp)
+ 		{
+ 		$lp = $_;
+ 		$lp =~ s!$self->{_regex_remotedir}!$self->{_localdir}!;
+ 		push(@$ra_lp, $lp);
+ 		}
+ 	return $ra_lp;
+ 	}
+#-------------------------------------------------
 1;
 #------------------------------------------------
 __END__
@@ -183,22 +265,22 @@ Net::DownloadMirror - Perl extension for mirroring a remote location via FTP to 
 
   use Net::DownloadMirror;
   my $um = Net::DownloadMirror->new(
- 	ftpserver		=> "my_ftp.hostname.com",
- 	user		=> "my_ftp_user_name",
- 	pass		=> "my_ftp_password",
+ 	ftpserver		=> 'my_ftp.hostname.com',
+ 	user		=> 'my_ftp_user_name',
+ 	pass		=> 'my_ftp_password',
  	);
  $um->Download();
  
  or more detailed
  my $md = Net::DownloadMirror->new(
- 	ftpserver		=> "my_ftp.hostname.com",
- 	user		=> "my_ftp_user_name",
- 	pass		=> "my_ftp_password",
- 	localdir		=> "home/nameA/homepageA",
- 	remotedir	=> "public",
+ 	ftpserver		=> 'my_ftp.hostname.com',
+ 	user		=> 'my_ftp_user_name',
+ 	pass		=> 'my_ftp_password',
+ 	localdir		=> 'home/nameA/homepageA',
+ 	remotedir	=> 'public',
  	debug		=> 1 # 1 for yes, 0 for no
  	timeout		=> 60 # default 30
- 	delete		=> "enable" # default "disabled"
+ 	delete		=> 'enable' # default 'disabled'
  	connection	=> $ftp_object, # default undef
 # "exclusions" default empty arrayreferences []
  	exclusions	=> ["private.txt", "Thumbs.db", ".sys", ".log"],
@@ -224,46 +306,62 @@ But there are not in principle any limits.
 
 =head1 Constructor and Initialization
 
-=item (object)new(options)
+=item (object) new (options)
  Net::DownloadMirror is a derived class from Net::MirrorDir.
  For detailed information about constructor or options
  read the documentation of Net::MirrorDir.
 
 =head2 methods
 
-=item (1)_Init(%arg)
+=item (1) _Init (%arg)
  This function is called by the constructor.
  You do not need to call this function by yourself.
 
-=item (1|0)Downlaod(void)
+=item (1|0) Downlaod (void)
  Call this function for mirroring automatically, recommended!!!
 
-=item (ref_hash_modified_files)CheckIfModified(ref_list_local_files)
+=item (ref_hash_modified_files) CheckIfModified (ref_array_local_files)
  Takes a hashreference of remoe filenames to compare the last modification time,
  which is stored in a file, named by the attribute "filename", while downloading. 
- Returns a reference of a list.
+ Returns a reference of a array.
 
-=item (1|0)StoreFiles(ref_array_paths)
+=item (1|undef) UpdateLastModified (ref_array_remote_files)
+ Update the stored modified-times of the remote files.
+
+=item (1|0) StoreFiles (ref_array_paths)
  Takes a arrayreference of remote-paths to download via FTP.
 
-=item (1|0)MakeDirs(ref_array_paths)
+=item (1|0) MakeDirs (ref_array_paths)
  Takes a arrayreference of directories to make in the local directory.
 
-=item (1|0)DeleteFiles(ref_array_paths)
+=item (1|0) DeleteFiles (ref_array_paths)
  Takes a arrayreference of files to delete in the local directory.
 
-=item (1|0)RemoveDirs(ref_array_paths)
+=item (1|0) RemoveDirs (ref_array_paths)
  Takes a arrayreference of directories to remove in the local directory.
 
-=head2 optional optiones
+=item (1) CleanUp (ref_array_paths)
+ Takes a arrayreference of directories to compare with the keys 
+ in the {_last_modified} hash. Is the key not in the array or
+ the value is incorrect, the key will be deleted.
+
+=item (ref_array_local_paths) RtoL (ref_array_remote_paths)
+ Takes a arrayreference of Remotepathnames and returns
+ a arrayreference of the corresponding Localpathnames.
+
+=item (ref_array_remote_paths) LtoR (ref_array_local_paths)
+ Takes a arrayreference of Localpathnames and returns
+ a arrayreference of the corresponding Remotepathnames.
+
+=head2 optional options
 
 =item filename
  The name of the file in which the last modified times will be stored.
- default = "lastmodified_remote"
+ default = 'lastmodified_remote'
 
  =item delete
- When directories or files are to be deleted = "enable"
- default = "disabled"
+ When directories or files are to be deleted = 'enable'
+ default = 'disabled'
 
 =head2 EXPORT
 
@@ -271,14 +369,14 @@ None by default.
 
 =head1 SEE ALSO
 
-Net::MirrorDir
-Net::UploadMirror
-Tk::Mirror
-http://www.planet-interkom.de/t.knorr/index.html
+ Net::MirrorDir
+ Net::UploadMirror
+ Tk::Mirror
+ http://freenet-homepage.de/torstenknorr/index.html
 
 =head1 FILES
 
- Net::MirrorDir
+ Net::MirrorDir 0.19
  Storable
  File::Path
 
@@ -303,3 +401,4 @@ it under the same terms as Perl itself, either Perl version 5.9.2 or,
 at your option, any later version of Perl 5 you may have available.
 
 =cut
+
--- t/Net-DownloadMirror.t
+++ t/Net-DownloadMirror.t
@@ -6,7 +6,7 @@
 # change 'tests => 1' to 'tests => last_test_to_print';
 
 # use Test::More "no_plan";
- use Test::More tests => 64;
+ use Test::More tests => 97;
 BEGIN { use_ok('Net::DownloadMirror') };
 
 #########################
@@ -14,61 +14,63 @@ BEGIN { use_ok('Net::DownloadMirror') };
 # Insert your test code below, the Test::More module is use()ed here so read
 # its man page ( perldoc Test::More ) for help writing this test script.
 #-------------------------------------------------
+ use_ok('Storable');
 # this section will test the methods in the baseclass Net::MirrorDir
  my $mirror = Net::DownloadMirror->new(
- 	localdir		=> "TestA",
- 	remotedir	=> "TestD",
- 	ftpserver		=> "www.net.de",
+ 	localdir		=> 'TestA',
+ 	remotedir	=> 'TestD',
+ 	ftpserver		=> 'www.net.de',
  	user		=> 'e-mail@address.de',
- 	pass		=> "xyz", 	
+ 	pass		=> 'xyz', 	
  	);
 #-------------------------------------------------
-# can we use the functions from base class Net::MirrorDir
- isa_ok($mirror, "Net::MirrorDir");
- can_ok($mirror, "Connect");
- can_ok($mirror, "IsConnection");
- can_ok($mirror, "Quit");
- can_ok($mirror, "ReadLocalDir");
+# can we use the methods of the base class Net::MirrorDir
+ isa_ok($mirror, 'Net::MirrorDir');
+ can_ok($mirror, 'Connect');
+ can_ok($mirror, 'IsConnection');
+ can_ok($mirror, 'Quit');
+ can_ok($mirror, 'ReadLocalDir');
  ok($mirror->ReadLocalDir('.'));
- can_ok($mirror, "ReadRemoteDir");
- can_ok($mirror, "LocalNotInRemote");
+ can_ok($mirror, 'ReadRemoteDir');
+ can_ok($mirror, 'LocalNotInRemote');
  ok($mirror->LocalNotInRemote({}, {}));
- can_ok($mirror, "RemoteNotInLocal");
+ can_ok($mirror, 'RemoteNotInLocal');
  ok($mirror->RemoteNotInLocal({}, {}));
- can_ok($mirror, "AUTOLOAD");
- can_ok($mirror, "DESTROY");
+ can_ok($mirror, 'AUTOLOAD');
+ can_ok($mirror, 'DESTROY');
 #-------------------------------------------------
- ok($mirror->Set_Remotedir("TestA"));
- ok("TestA" eq $mirror->get_remotedir());
- ok($mirror->SetLocaldir("TestB"));
+# test attribute 'remotedir' and 'localdir'
+ ok($mirror->Set_Remotedir('TestA'));
+ ok('TestA' eq $mirror->get_remotedir());
+ ok($mirror->SetLocaldir('TestB'));
  ok("TestB" eq $mirror->GetLocaldir());
 #-------------------------------------------------
-# test attribute "subset"
+# test attribute 'subset'
  ok($mirror->SetSubset([]));
- ok($mirror->AddSubset("test_1"));
- ok("test_1" eq $mirror->GetSubset()->[0]);
- ok($mirror->AddSubset("test_2"));
- ok("test_2" eq $mirror->GetSubset()->[1]);
- ok($mirror->add_subset("test_3"));
- ok("test_3" eq $mirror->get_subset()->[2]);
+ ok($mirror->AddSubset('test_1'));
+ ok('test_1' eq $mirror->GetSubset()->[0]);
+ ok($mirror->AddSubset('test_2'));
+ ok('test_2' eq $mirror->GetSubset()->[1]);
+ ok($mirror->add_subset('test_3'));
+ ok('test_3' eq $mirror->get_subset()->[2]);
  my $count = 0;
  for my $regex (@{$mirror->{_regex_subset}})
  	{
- 	for("---test_1---", "---test_2---", "---test_3---")
+ 	for('---test_1---', '---test_2---', '---test_3---')
  		{
  		$count++ if(/$regex/)
  		}
  	}
  ok($count == 3);
 #-------------------------------------------------
-# test attribute "exclusions"
+# test attribute 'exclusions'
  ok($mirror->SetExclusions([qr/test_1/]));
  ok($mirror->AddExclusions(qr/test_2/));
  ok($mirror->add_exclusions(qr/test_3/));
  $count = 0;
  for my $regex (@{$mirror->get_regex_exclusions()})
  	{
- 	for("xxxtest_1xxx", "xxxtest_2xxx", "xxxtest_3xxx")
+ 	for('xxxtest_1xxx', 'xxxtest_2xxx', 'xxxtest_3xxx')
  		{
  		$count++ if(/$regex/);
  		}
@@ -76,36 +78,80 @@ BEGIN { use_ok('Net::DownloadMirror') };
  ok($count == 3);
 #-------------------------------------------------
 # tests for Net::DownloadMirror methods
- isa_ok($mirror, "Net::MirrorDir");
- isa_ok($mirror, "Net::DownloadMirror");
- can_ok($mirror, "_Init");
+ isa_ok($mirror, 'Net::MirrorDir');
+ isa_ok($mirror, 'Net::DownloadMirror');
+ can_ok($mirror, '_Init');
  ok($mirror->_Init());
- can_ok($mirror, "Download");
- can_ok($mirror, "StoreFiles");
+ can_ok($mirror, 'Download');
+ can_ok($mirror, 'StoreFiles');
  ok(!$mirror->StoreFiles([]));
- can_ok($mirror, "CheckIfModified");
+ can_ok($mirror, 'CheckIfModified');
  ok($mirror->CheckIfModified({}));
- can_ok($mirror, "MakeDirs");
+ can_ok($mirror, 'MakeDirs');
  ok(!$mirror->MakeDirs([]));
- can_ok($mirror, "DeleteFiles");
- ok($mirror->SetDelete("enable"));
+ can_ok($mirror, 'DeleteFiles');
+ ok($mirror->SetDelete('enable'));
  ok(!$mirror->DeleteFiles([]));
- can_ok($mirror, "RemoveDirs");
+ can_ok($mirror, 'RemoveDirs');
  ok(!$mirror->RemoveDirs([]));
- ok($mirror->SetDelete("disabled"));
+ ok($mirror->SetDelete('disabled'));
+ can_ok($mirror, 'UpdateLastModified');
 #-------------------------------------------------
-# tests for "filename"
- ok($mirror->GetFileName() eq "lastmodified_remote");
- ok($mirror->SetFileName("modtime"));
- ok($mirror->GetFileName() eq "modtime");
- ok(unlink("lastmodified_remote"));
+# tests for method CleanUp([...])
+ can_ok($mirror, 'CleanUp');
+ ok(store(
+ 	{
+ 	keyA	=> 1234,
+ 	keyB	=> 'xyz',
+ 	keyC	=> undef,
+ 	keyD	=> 56789
+ 	}, 'times'));
+ ok($mirror->SetFileName('times'));
+ ok($mirror->CleanUp(['keyA', 'keyB', 'keyC']));
+ ok(defined($mirror->{_last_modified}{keyA}));
+ ok(!defined($mirror->{_last_modified}{keyB})) for(qw/keyB keyC keyD/);
+ ok(unlink('times'));
+#-------------------------------------------------
+# test for method (ref_array_local_paths) RtoL (ref_array_remote_paths)
+ can_ok($mirror, 'RtoL');
+ ok($mirror->SetLocalDir('Home'));
+ ok($mirror->SetRemoteDir('www.server.de/public'));
+ ok(my $ra_ld = $mirror->RtoL(
+ 	[
+ 	'www.server.de/public/pageA',
+ 	'www.server.de/public/pageB',
+ 	'www.server.de/public/pageC',
+ 	]));
+ ok($ra_ld->[0] eq 'Home/pageA');
+ ok($ra_ld->[1] eq 'Home/pageB');
+ ok($ra_ld->[2] eq 'Home/pageC');
+#-------------------------------------------------
+# tests for method (ref_array_remote_paths) LtoR (ref_array_local_paths)
+ can_ok($mirror, 'LtoR');
+ ok($mirror->SetRemoteDir('ftp.org.de/html/usr/local/name'));
+ ok($mirror->SetLocalDir('C:\\Doc/html/homepage'));
+ ok(my $ra_rd = $mirror->LtoR(
+ 	[
+ 	'C:\\Doc/html/homepage/index.html',
+	'C:\\Doc/html/homepage/style.css',
+	'C:\\Doc/html/homepage/info.html',
+ 	]));
+ ok($ra_rd->[0] eq 'ftp.org.de/html/usr/local/name/index.html');
+ ok($ra_rd->[1] eq 'ftp.org.de/html/usr/local/name/style.css');
+ ok($ra_rd->[2] eq 'ftp.org.de/html/usr/local/name/info.html');
+#-------------------------------------------------
+# tests for attribute 'filename'
+ ok($mirror->SetFileName('lastmodified_remote'));
+ ok($mirror->GetFileName() eq 'lastmodified_remote');
+ ok($mirror->SetFileName('modtime'));
+ ok($mirror->GetFileName() eq 'modtime');
  ok(unlink("modtime"));
 #-------------------------------------------------
-# tests for "delete"
- ok("disabled" eq $mirror->GetDelete());
+# tests for attribute 'delete'
+ ok('disabled' eq $mirror->GetDelete());
 #-------------------------------------------------
-# tests for "current_modified"
- ok("HASH" eq ref($mirror->GetCurrent_Modified()));
+# tests for attribute 'current_modified'
+ ok('HASH' eq ref($mirror->GetCurrent_Modified()));
 #-------------------------------------------------
  SKIP:
  	{
@@ -117,9 +163,8 @@ BEGIN { use_ok('Net::DownloadMirror') };
  		pass		=> 'create-soft@tiscali.de', 	
  		exclusions	=> ['CHECKSUMS']
  		);
- 	skip("no tests with www.cpan.org\n", 8) unless($m->Connect());
- 	ok($m->Download());
- 	ok(-f $_) for(
+ 	skip("no tests with www.cpan.org\n", 16) unless($m->Connect());
+ 	my $ra_lf = [
  		'TestA/TestB/TestC/Dir1/test1.txt',
  		'TestA/TestB/TestC/Dir2/test2.txt',
  		'TestA/TestB/TestC/Dir2/test2.subset',
@@ -127,7 +172,15 @@ BEGIN { use_ok('Net::DownloadMirror') };
  		'TestA/TestB/TestC/Dir4/test4.txt',
  		'TestA/TestB/TestC/Dir4/test4.exclusions',
  		'TestA/TestB/TestC/Dir5/test5.txt'
- 		);
+ 		];
+ 	ok($m->UpdateLastModified($m->LtoR($ra_lf)));
+ 	ok(
+ 		defined($m->{_last_modified}{$_})
+ 		&&
+ 		$m->{_last_modified}{$_} =~ m/^\d+$/
+ 		) for(@{$m->LtoR($ra_lf)});
+ 	ok($m->Download());
+ 	ok(-f $_) for(@$ra_lf);
  	}
 #-------------------------------------------------
  SKIP:
@@ -165,11 +218,3 @@ BEGIN { use_ok('Net::DownloadMirror') };
 	select($oldfh);
  	}
 #-------------------------------------------------
-
-
-
-
-
-
-
-
